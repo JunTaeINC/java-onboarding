b50d37017ba3286b5693172d5dc1fbe0b8a1edc6
JunTaeINC <juntea3523@naver.com>
2022-11-23 22:54:36 +0900

    Pb 2 ) lengthCheck -> lengthZeroOrOne 메서드명 변경

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,16 +1,17 @@
 package onboarding;
 
 public class Problem2 {
-    private static final String EXCEPTION = "예외 발생";
+
     public static String solution(String cryptogram) {
 
     }
 
-    public boolean lengthCheck(String testObject){
-        return testObject.length() != 0 && testObject.length() != 1;
+    public boolean lengthZeroOrOne(String testObject) {
+        return testObject.length() == 0 || testObject.length() == 1;
     }
 
     static boolean validCheck(String testObject) {
         return testObject.matches("(^[a-z]*$)");
     }
+
 }
