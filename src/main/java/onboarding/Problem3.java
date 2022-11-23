348e25437b587d908d67f66d1c6f2d839ce5e7e1
JunTaeINC <juntea3523@naver.com>
2022-11-23 23:20:22 +0900

    Pb 3 ) checkingNumber , solution 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,26 @@
 package onboarding;
 
 public class Problem3 {
+    private final static char THREE = '3';
+    private final static char SIX = '6';
+    private final static char NINE = '9';
+
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        return checkingNumber(number);
+    }
+
+    static int checkingNumber(int testNumber) {
+        int count = 0;
+
+        for (int number = 0; number <= testNumber; number++) {
+            String stringNumber = String.valueOf(number);
+            for (int length = 0; length < stringNumber.length(); length++) {
+                if (stringNumber.charAt(length) == THREE || stringNumber.charAt(length) == SIX ||
+                        stringNumber.charAt(length) == NINE) {
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 }
