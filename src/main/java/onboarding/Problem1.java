f067d959473dcb7b6bfd57fccbfb3f2f678a7d87
JunTaeINC <juntea3523@naver.com>
2022-11-23 21:42:24 +0900

    Pb 1 ) pageValidCheck 페이지 예외 검사 기능 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,17 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+    //리스트/배열 pobi와 crong이 주어질 때, 포비가 이긴다면 1, 크롱이 이긴다면 2, 무승부는 0, 예외사항은 -1로 return
+    //왼쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
+    private final int MAX_RANGE = 400;
+    private final int MIN_RANGE = 1;
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        Integer a = 123;
+
+    }
+
+    public boolean pageValidCheck(List<Integer> pageNumbers){
+        return pageNumbers.get(1) - pageNumbers.get(0) == 1 &&
+                MIN_RANGE <= pageNumbers.get(0) && MAX_RANGE >= pageNumbers.get(1);
     }
 }
