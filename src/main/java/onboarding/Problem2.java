2748feecd2261aa77a806c0b176391a05da24735
JunTaeINC <juntea3523@naver.com>
2022-11-23 23:00:16 +0900

    Pb 2 ) validCheck 글자 길이 확인 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -3,15 +3,17 @@ package onboarding;
 public class Problem2 {
 
     public static String solution(String cryptogram) {
-
     }
 
-    public boolean lengthZeroOrOne(String testObject) {
+    static boolean lengthZeroOrOne(String testObject) {
         return testObject.length() == 0 || testObject.length() == 1;
     }
 
     static boolean validCheck(String testObject) {
-        return testObject.matches("(^[a-z]*$)");
+        if (testObject.matches("(^[a-z]*$)") && 0 < testObject.length() && testObject.length() < 1001) {
+            return true;
+        }
+        return false;
     }
 
 }
