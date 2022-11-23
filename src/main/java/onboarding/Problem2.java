f0eaf7112b9b20443d0ed27d167e76615a503bfd
JunTaeINC <juntea3523@naver.com>
2022-11-23 22:28:10 +0900

    Pb 2 ) validCheck 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,15 @@
 package onboarding;
 
 public class Problem2 {
+    private static final String EXCEPTION = "예외 발생";
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+
+    }
+
+    static String  validCheck(String testObject) {
+        if (!testObject.matches("(^[a-z]*$)")){
+            System.out.println(EXCEPTION);
+        }
+        return testObject;
     }
 }
