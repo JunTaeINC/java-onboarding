ba8a88e320caf5a9fa607cd73c3fa41a4453cc19
JunTaeINC <juntea3523@naver.com>
2022-11-24 23:13:21 +0900

    Pb 7 ) makeHashMap 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,20 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
     }
+
+    static Map<String, Integer> makeHashMap(List<List<String>> friends) {
+        Map<String, Integer> friendAndScore = new HashMap<>();
+        for (List<String> friend : friends) {
+            friendAndScore.put(friend.get(0), 0);
+            friendAndScore.put(friend.get(1), 0);
+        }
+        return friendAndScore;
+    }
+
 }
