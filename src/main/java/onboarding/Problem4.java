410eea8bb0641484a38f26ebee952454f6cd8418
JunTaeINC <juntea3523@naver.com>
2022-11-24 13:15:39 +0900

    Pb 4 ) checkString 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,15 @@
 package onboarding;
 
 public class Problem4 {
+
+    private static final String A_TO_z = "(^[A-z]*$)";
+
     public static String solution(String word) {
         String answer = "";
         return answer;
+    }
+
+    static boolean checkString(String testObject) {
+        return testObject.matches(A_TO_z);
     }
 }
