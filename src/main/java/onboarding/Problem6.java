f008735bbd52242cdd796222a83f303ac97f58f6
JunTaeINC <juntea3523@naver.com>
2022-11-24 22:32:48 +0900

    Pb 6 ) solution 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,6 +1,7 @@
 package onboarding;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Problem6 {
@@ -13,15 +14,33 @@ public class Problem6 {
 
 
     public static List<String> solution(List<List<String>> forms) {
-
+        List<String> answer = new ArrayList<>();
+        for (List<String> form : forms) {
+            List<String> nickNameDuplicationList = makeNickNameDuplication(forms);
+            if (checkValid(form)) {
+                String nickName = form.get(1);
+                for (int length = 0; length < nickName.length() - 1; length++) {
+                    nickNameDuplicationList.remove(nickName.substring(length, length + 2));
+                }
+                for (int length = 0; length < nickName.length() - 1; length++) {
+                    if (nickNameDuplicationList.contains(nickName.substring(length, length + 2))) {
+                        answer.add(form.get(0));
+                    }
+                }
+            } else {
+                throw new IllegalArgumentException();
+            }
+        }
+        Collections.sort(answer);
+        return answer;
     }
 
     static List<String> makeNickNameDuplication(List<List<String>> forms) {
         List<String> nickNameDuplication = new ArrayList<>();
         for (List<String> form : forms) {
             String nickName = form.get(1);
-            for (int length = 0; length < nickName.length() - 2; length++) {
-                nickNameDuplication.add(nickName.substring(length, length + 1));
+            for (int length = 0; length < nickName.length() - 1; length++) {
+                nickNameDuplication.add(nickName.substring(length, length + 2));
             }
         }
         return nickNameDuplication;
@@ -34,7 +53,6 @@ public class Problem6 {
     static boolean checkEmailRange(List<String> form) {
         return E_MAIL_MIN_RANGE <= form.get(0).length() && form.get(0).length() < E_MAIL_MAX_RANGE;
     }
-
 
 
     static boolean checkNickNameRange(List<String> form) {
