af43f54189eaeb98efc05f74e28e4e2e33b389b9
JunTaeINC <juntea3523@naver.com>
2022-11-24 23:37:52 +0900

    Pb 7 ) userFriendGetScore 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,14 +4,26 @@ import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        Map<String , Integer> nameAndScore = makeHashMap(friends);
-        putVisitors(nameAndScore , visitors);
+        Map<String, Integer> nameAndScore = makeHashMap(friends);
 
+        List<String> userFriendList = makeUserFriendList(user, friends);
+        putVisitors(nameAndScore, visitors);
+        user
+    }
 
+    static Map<String, Integer> userFriendGetScore(Map<String, Integer> nameAndScore,List<List<String>> friends , List<String> userFriendList) {
+        for (String userFriend : userFriendList) {
+            for(List<String> friend : friends) {
+                if(Objects.equals(friend.get(0), userFriend)) {
+                    nameAndScore.put(friend.get(1) , nameAndScore.get(friend.get(1)) + 10);
+                }
+            }
+        }
+        return nameAndScore;
     }
 
     static List<String> makeUserFriendList(String user, List<List<String>> friendsList) {
-        List<String> userFriendList  = new ArrayList<>();
+        List<String> userFriendList = new ArrayList<>();
         for (List<String> friends : friendsList) {
             if (Objects.equals(friends.get(1), user)) {
                 userFriendList.add(friends.get(0));
@@ -20,13 +32,12 @@ public class Problem7 {
         return userFriendList;
     }
 
-    static Map<String, Integer> putVisitors(Map<String, Integer> nameAndScore , List<String> visitors) {
-        for (String visitor : visitors){
+    static Map<String, Integer> putVisitors(Map<String, Integer> nameAndScore, List<String> visitors) {
+        for (String visitor : visitors) {
             if (nameAndScore.containsKey(visitor)) {
-                nameAndScore.put(visitor , nameAndScore.get(visitor) + 1);
-            }
-            else {
-                nameAndScore.put(visitor , 0);
+                nameAndScore.put(visitor, nameAndScore.get(visitor) + 1);
+            } else {
+                nameAndScore.put(visitor, 0);
             }
         }
         return nameAndScore;
@@ -40,6 +51,6 @@ public class Problem7 {
         }
         return nameAndScore;
     }
-    static
 
+    static
 }
