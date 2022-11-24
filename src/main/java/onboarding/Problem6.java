eb9a4a86218af4006d4627605623e61bc43620bf
JunTaeINC <juntea3523@naver.com>
2022-11-24 21:47:16 +0900

    Pb 6 ) checkNickNameRange 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -3,13 +3,25 @@ package onboarding;
 import java.util.List;
 
 public class Problem6 {
+
+    private static final String E_MAIL = "@email.com";
+    private static final int NICKNAME_MIN_RANGE = 1;
+    private static final int NICKNAME_MAX_RANGE = 20;
+
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
     }
 
+    static boolean checkNickNameRange(String nickName) {
+        if (NICKNAME_MIN_RANGE <= nickName.length() && NICKNAME_MAX_RANGE < 20) {
+            return true;
+        }
+        return false;
+    }
+
     static boolean checkEmail(String email) {
-        if (email.endsWith("@email.com")) {
+        if (email.endsWith(E_MAIL)) {
             return true;
         }
         return false;
