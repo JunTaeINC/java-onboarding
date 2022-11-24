32eabc09393159f8169543b3cfa955d0fc48d65b
JunTaeINC <juntea3523@naver.com>
2022-11-24 21:49:13 +0900

    Pb 6 ) checkEmailRange 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -7,10 +7,20 @@ public class Problem6 {
     private static final String E_MAIL = "@email.com";
     private static final int NICKNAME_MIN_RANGE = 1;
     private static final int NICKNAME_MAX_RANGE = 20;
+    private static final int E_MAIL_MIN_RANGE = 11;
+    private static final int E_MAIL_MAX_RANGE = 20;
+
 
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    static boolean checkEmailRange(String email) {
+        if (E_MAIL_MIN_RANGE <= email.length() && email.length() < E_MAIL_MAX_RANGE) {
+            return true;
+        }
+        return false;
     }
 
     static boolean checkNickNameRange(String nickName) {
