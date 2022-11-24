3ca324250538e3016d9a08e011739e3a127283de
JunTaeINC <juntea3523@naver.com>
2022-11-24 13:39:56 +0900

    Pb 4 ) convertString, solution 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,13 +3,38 @@ package onboarding;
 public class Problem4 {
 
     private static final String A_TO_z = "(^[A-z]*$)";
+    private static final String EXCEPTION = "예외 발생";
+    private static final int a = 97;
+    private static final int z = 122;
+    private static final int A = 65;
+    private static final int Z = 90;
+
 
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        if (!checkString(word)) {
+            return convertString(word);
+        } else {
+            return EXCEPTION;
+        }
+    }
+
+    static String convertString(String words) {
+        StringBuilder answer = new StringBuilder();
+        for (int length = 0; length < words.length(); length++) {
+            char word = words.charAt(length);
+            if ((int) word <= z && a <= (int) word) {
+                answer.append((char) (z - (int) word + a));
+            } else if ((int) word <= Z && A <= (int) word) {
+                answer.append((char) (Z - (int) word + A));
+            } else {
+                answer.append(word);
+            }
+        }
+        return answer.toString();
     }
 
     static boolean checkString(String testObject) {
         return testObject.matches(A_TO_z);
     }
+
 }
